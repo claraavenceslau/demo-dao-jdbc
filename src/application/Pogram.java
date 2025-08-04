89fae4d334ecca22d92a670eaf8c6e494b73550e
Maria Clara <mclaravenceslaum@gmail.com>
2025-08-03 22:27:53 -0300

    findByDepartment implementation

--- src/application/Pogram.java
+++ src/application/Pogram.java
@@ -7,6 +7,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class Pogram {
@@ -14,10 +15,17 @@ public class Pogram {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TEST 1 FINDBYID ===");
+        System.out.println("==== TEST 1 FINDBYID ====");
         System.out.printf("Digit employee id: ");
-        int id = new Scanner(System.in).nextInt();
-        Seller seller = sellerDao.findById(id);
+        int id;
+        Seller seller = sellerDao.findById(id = new Scanner(System.in).nextInt());
         System.out.println(seller);
+
+        System.out.println("\n==== TEST 2 FINDBYDEPARTMENT ====");
+        Department dep = new Department(2, null);
+        List<Seller> sellers = sellerDao.findByDepartment(dep);
+        for (Seller s : sellers) {
+            System.out.println(s);
+        }
     }
 }
