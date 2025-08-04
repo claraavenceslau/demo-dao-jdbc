cba42c050340170cd130f7876078577487204e6c
Maria Clara <mclaravenceslaum@gmail.com>
2025-08-03 22:47:13 -0300

    findAll implementation

--- src/application/Pogram.java
+++ src/application/Pogram.java
@@ -15,15 +15,21 @@ public class Pogram {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("==== TEST 1 FINDBYID ====");
+        System.out.println("==== TEST 1 FindById ====");
         System.out.printf("Digit employee id: ");
         int id;
         Seller seller = sellerDao.findById(id = new Scanner(System.in).nextInt());
         System.out.println(seller);
 
-        System.out.println("\n==== TEST 2 FINDBYDEPARTMENT ====");
+        System.out.println("\n==== TEST 2 FindByDepartment ====");
         Department dep = new Department(2, null);
         List<Seller> sellers = sellerDao.findByDepartment(dep);
+        for (Seller s : sellers) {
+            System.out.println(s);
+        }
+
+        System.out.println("\n==== TEST 3 FindAll ====");
+        sellers = sellerDao.findAll();
         for (Seller s : sellers) {
             System.out.println(s);
         }
